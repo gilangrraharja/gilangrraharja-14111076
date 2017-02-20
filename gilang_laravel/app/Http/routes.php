781ebea7f7c2acd95967b99fcafbfda603b7ae69
gilangrraharja <gilangrizkia331@gmail.com>
2017-02-20 21:01:32 +0700

    tugas Eloquent CRUD

--- gilang_laravel/app/Http/routes.php
+++ gilang_laravel/app/Http/routes.php
@@ -1,4 +1,5 @@
 <?php
+use App\mahasiswa;
 
 /*
 |--------------------------------------------------------------------------
@@ -14,3 +15,35 @@
 Route::get('/', 'ViewController@index');
 Route::get('pengaduan', 'ViewController@pengaduan');
 Route::get('login', 'ViewController@login');
+Route::get('tambah', function(){
+	$mahasiswa = new mahasiswa;
+	$mahasiswa -> nama = "gilang";
+	$mahasiswa -> no_id = "14111076";
+	$mahasiswa -> phone = "08122234711";
+	$mahasiswa -> pengaduan = "error login";
+	$mahasiswa -> save ();});
+
+Route::get('ubah', function(){
+	$mahasiswa = new mahasiswa;
+	$mahasiswa -> nama = "gilang";
+	$mahasiswa -> no_id = "14111076";
+	$mahasiswa -> phone = "08122234711";
+	$mahasiswa -> pengaduan = "sukses";
+	$mahasiswa -> save ();});
+
+Route::get('tampil', function(){
+	$mahasiswa = mahasiswa::all();
+	foreach ($mahasiswa as $mhs) {
+		echo "<b>nama</b> :";
+		echo $mhs->nama;
+		echo "<b>no_id</b> :";
+		echo $mhs->no_id;
+		echo "<b>phone</b> :";
+		echo $mhs->phone;
+		echo "<b>pengaduan</b> :";
+		echo $mhs->pengaduan;}
+	});
+
+Route::get('hapus', function(){
+	$mahasiswa = mahasiswa::find(1);
+	$mahasiswa -> delete();});
